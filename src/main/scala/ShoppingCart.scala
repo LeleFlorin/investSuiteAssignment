3383ad051d9e70175b347a7d3cc9fb087dee9ce8
florin.lele <flele@deloittece.com>
2021-12-09 02:11:41 +0200

    [FL] refactoring

--- src/main/scala/ShoppingCart.scala
+++ src/main/scala/ShoppingCart.scala
@@ -1,8 +1,6 @@
 import scala.io.StdIn
 
 object ShoppingCart extends App {
-
-
   def executeCLI(command: List[String], shoppingService: ShoppingService): Unit = command match {
     case List("add_product", name) => println(s"status: ${shoppingService.addProduct(name)}")
     case List("remove_product", name) => println(s"status: ${shoppingService.removeProduct(name)}")
@@ -11,7 +9,6 @@ object ShoppingCart extends App {
       println(s"status: $status, products: $products")
     case x => println(s"Invalid command: '${x.mkString(" ")}'. Available commands are: 'add_product <prod_name>', 'remove_product <prod_name>', 'get_cart'")
   }
-
 
   val shoppingService = ShoppingServiceImpl(ShoppingRepoImpl())
   LazyList.from(1).foreach(_ => executeCLI(StdIn.readLine.split(" ").toList, shoppingService))
